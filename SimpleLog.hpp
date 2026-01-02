88494b628f327294268a571496b19d17ce2e751e
lizhuoran <liZhuoran1998@outlook.com>
2026-01-02 18:10:38 +0800

    Append Buffer::Push && Buffer::Pop

--- SimpleLog.hpp
+++ SimpleLog.hpp
@@ -5,6 +5,7 @@
 #include <filesystem>
 #include <fstream>
 #include <deque>
+#include <mutex>
 
 namespace Report {
     static inline std::atomic<unsigned long long> report_uid = 0;
@@ -63,7 +64,7 @@ namespace Report {
         [[nodiscard]] std::string GetMessage(OutType type = OutType::Default) const {
             std::string prefix = LevelToString(m_level);
             // 这里的颜色是真彩色, 并不是所有终端都支持(老一点终端可能不支持)
-            // todo: 之后可以专门封装一个 ColorWapper(std::string text, int r, int g, int b) 来支持不同终端中的颜色显示
+            // todo: 之后可以专门封装一个 ColorWrapper(std::string text, int r, int g, int b) 来支持不同终端中的颜色显示
             if (type == OutType::Console) {
                 switch (m_level) {
                     case Level::Temp: /* 没有颜色, 不做任何设置*/                                break;
@@ -82,9 +83,9 @@ namespace Report {
         void SetUID(const size_t uid) { m_uid = uid; }
         [[nodiscard]] size_t GetCatalogue() const { return m_uid; }
         [[nodiscard]] size_t GetUID() const { return m_catalogue_uid; }
-        bool IsNeedWriteToFile() const { return m_level != Level::Temp; }
+        [[nodiscard]] bool IsNeedWriteToFile() const { return m_level != Level::Temp; }
     private:
-        std::string m_msg { "" };
+        std::string m_msg;
         Level m_level { Level::Info };
         size_t m_catalogue_uid { 0 };
         size_t m_uid { 0 };
@@ -98,28 +99,96 @@ namespace Report {
     Message MakeMessage(const Level level, std::format_string<Args...> fmt, Args&&... args) {;
         return MakeMessage(std::format(fmt, std::forward<Args>(args)...), level);
     }
+
     struct Buffer {
     public:
         static constexpr std::format_string<size_t> DefaultLogFileNameFormat = "Log_Buffer_{}.log";
     public:
         Buffer() : m_uid(report_uid++) {
-            m_file_path = std::filesystem::current_path() / std::format(DefaultLogFileNameFormat, std::move(m_uid));
+            m_file_path = std::filesystem::current_path() / std::format(DefaultLogFileNameFormat, static_cast<size_t>(m_uid));
             m_file_stream.open(m_file_path, std::ios::out | std::ios::app);
             if (!m_file_stream.is_open()) {
                 Message err_msg = MakeMessage(Level::Error, "Buffer_{} failed to open log file: {}", m_uid, m_file_path.string());
                 m_messages.push_back(std::move(err_msg));
             }
         };
+
+        Buffer(const Buffer&) = delete;
+        Buffer& operator=(const Buffer&) = delete;
+
+        Buffer(Buffer&& buffer) noexcept
+            : m_uid(buffer.m_uid),
+              m_file_path(std::move(buffer.m_file_path)),
+              m_file_stream(std::move(buffer.m_file_stream)),
+              m_messages(std::move(buffer.m_messages)) {}
+
+        Buffer& operator=(Buffer&& other) noexcept {
+            m_uid = other.m_uid;
+            m_file_path = std::move(other.m_file_path);
+            m_file_stream = std::move(other.m_file_stream);
+            m_messages = std::move(other.m_messages);
+            return *this;
+        }
+
+        ~Buffer() {
+            if (m_file_stream.is_open()) {
+                m_file_stream.close();
+            }
+        }
+
+    public:
+        void Push(const Message& msg) {
+            std::unique_lock ulock(m_mutex);
+            m_messages.push_back(msg);
+            if (m_file_stream.is_open()) {
+                m_file_stream << msg.GetMessage();
+            }
+        }
+
+        void Push(const std::string &msg, const Level level = Level::Info) {
+            Push(Message(msg, level));
+        }
+
+        void Push(const char* msg, const Level level = Level::Info) {
+            Push(Message(std::string(msg), level));
+        }
+
+        [[nodiscard]] const Message& Pop() const {
+            return m_messages.front();
+        }
+
+        [[nodiscard]] Message Top() {
+            return m_messages.front();
+        }
+
+        Message& operator[](const size_t index) noexcept {
+            std::unique_lock ulock (m_mutex);
+            return m_messages[index];
+        }
+        const Message& operator[](const size_t index) const noexcept { return m_messages[index]; }
+
+        [[nodiscard]] Message& At(const size_t index) {
+            std::unique_lock ulock (m_mutex);
+            if (index >= m_messages.size()) throw std::out_of_range("index out of range");
+            return m_messages[index];
+        }
+        [[nodiscard]] const Message& At(const size_t index) const {
+            if (index >= m_messages.size()) throw std::out_of_range("index out of range");
+            return m_messages[index];
+        }
     private:
         size_t m_uid { 0 };
         std::filesystem::path m_file_path {};
         std::fstream m_file_stream {};
         std::deque<Message> m_messages {};
+        std::mutex m_mutex;
     };
-    void Write(Level level, Buffer& buffer, const std::string& message);
+    static void Write(const Level level, Buffer& buffer, const std::string& message) {
+
+    }
 
     template <typename... Args>
-    void Write(Level level, Buffer& buffer, const std::format_string<Args...> format, Args&&... args) {;
+    static void Write(const Level level, Buffer& buffer, const std::format_string<Args...> format, Args&&... args) {;
         Write(level, buffer, std::vformat(format, std::make_format_args(std::forward<Args>(args)...)));
     }
 }
